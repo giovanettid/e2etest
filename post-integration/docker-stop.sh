ff213184431493bbc6e5da37f98d28c773592211
didier <didier.giovanetti@gmail.com>
2016-08-17 23:27:15 +0200

    remove docker images after test

--- post-integration/docker-stop.sh
+++ post-integration/docker-stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker stop webapp && docker rm webapp 
-docker stop mysqldb && docker rm mysqldb 
+docker rmi springbatch-sample
+docker stop webapp && docker rm webapp && docker rmi spring-boot-sample
+docker stop mysqldb && docker rm mysqldb && docker rmi mysql-sample 
